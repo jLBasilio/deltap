8a2315a4adba242975517bbdf7f01b20109b2882
Jeff Basilio <jlcbasilio@gmail.com>
2022-11-27 03:24:02 +0800

    Add db execute wrappers

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
 export const DB_NAME = 'deltap';
+export type ParamType = null | string | number;
+export type ParamArray = ParamType[];
+export interface QueryWithParams {
+  query: string;
+  params: ParamArray;
+}
---
+++ src/database/execute.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+import { ParamArray, QueryWithParams } from '@src/constants';
+import {
+  SQLError,
+  SQLTransaction,
+  SQLStatementErrorCallback,
+  WebSQLDatabase,
+  SQLResultSet
+} from 'expo-sqlite';
+
+function errorWrapper(
+  query: string,
+  params: ParamArray,
+  reject: Function
+): SQLStatementErrorCallback {
+  return (_: SQLTransaction, err: SQLError): boolean => {
+    console.log('\n\nQUERY ERROR!');
+    console.log(`${query} : [${params}]`);
+    console.log(err);
+    reject(err);
+    return true;
+  };
+}
+
+export async function executeOne(
+  db: WebSQLDatabase,
+  query: string,
+  params: ParamArray
+): Promise<any> {
+  return await new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        query,
+        params,
+        (_, res: SQLResultSet) => resolve(res),
+        errorWrapper(query, params, reject)
+      );
+    });
+  });
+}
+
+export async function executeMultiple(
+  db: WebSQLDatabase,
+  queryArray: QueryWithParams[]
+): Promise<any> {
+  console.log('creating');
+  return await new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      const results = queryArray.map(
+        async ({ query, params }) =>
+          await new Promise((txResolve, txReject) => {
+            tx.executeSql(
+              query,
+              params,
+              (_, res: SQLResultSet) => txResolve(res),
+              errorWrapper(query, params, txReject)
+            );
+          })
+      );
+
+      Promise.all(results)
+        .then((res) => resolve(res))
+        .catch((err) => reject(err));
+    });
+  });
+}
